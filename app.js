1fb06e38422ca6d8a51c96eda93d5d5e1d02c551
Paryej Alam <paryejalam01@gmail.com>
2026-03-11 01:26:58 +0530

    Refactor request handling for improved syntax

--- app.js
+++ app.js
@@ -1,26 +1,34 @@
 let todo = [];
-let req = prompt("Enter the your request");
-while(true){
-    if(req=="quit"){
+let req = prompt("Enter your request");
+
+while (true) {
+    if (req == "quit") {
         console.log("QUITTING APP");
         break;
     }
-    if(req=="list"){
-        console.log("-----TO DO LIST-----");
-        for(let i=0;i<todo.length;i++){
-            console.log(i,todo[i]);
+
+    else if (req == "list") {
+        console.log("----- TO DO LIST -----");
+        for (let i = 0; i < todo.length; i++) {
+            console.log(i, todo[i]);
         }
     }
-    if(req=="add"){
+
+    else if (req == "add") {
         let task = prompt("Enter the task you want to add");
         todo.push(task);
         console.log("Task added");
     }
-    if(req=="delete"){
-        let idx = prompt("please enter the task index");
-        todo. splice(idx, 1);
+
+    else if (req == "delete") {
+        let idx = parseInt(prompt("Enter the task index"));
+        todo.splice(idx, 1);
         console.log("Task deleted");
     }
-    req = prompt("Enter the your request");
-}
 
+    else {
+        console.log("Invalid request");
+    }
+
+    req = prompt("Enter your request");
+}
